7ea912055803d7b0c4f0ad6bc67582cf7d9813dd
Nikitajadhav2223 <95409583+Nikitajadhav2223@users.noreply.github.com>
2025-03-27 15:53:43 +0530

    update

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express =require('express');
-
+require('dotenv').config();
 const mongoose = require('mongoose');
 const routes = require('./Routes/index');
 
@@ -22,22 +22,37 @@ app.use((req, res, next)=>{
 app.use('/',routes);
 
 
-mongoose.connect(
-    'mongodb://127.0.0.1:27017/local',  // Specify your database name here
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }
-).then(success => {
-    console.log("✅ MongoDB Connected Successfully");
+// mongoose.connect(
+//     // 'mongodb://127.0.0.1:27017/local',  // Specify your database name here
 
-    app.listen(port, () => {
-        console.log(`Server is running on ${port}`);
-    });
 
-}).catch(error => {
-    console.log("❌ MongoDB Connection Error: " + error);
-});
+//     {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true
+//     }
+// ).then(success => {
+//     console.log("✅ MongoDB Connected Successfully");
+
+//     app.listen(port, () => {
+//         console.log(`Server is running on ${port}`);
+//     });
+
+// }).catch(error => {
+//     console.log("❌ MongoDB Connection Error: " + error);
+// });
+
+
+// Database Connection
+mongoose.connect( process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('✅ MongoDB Connected Successfully'))
+.catch(err => console.error('❌ MongoDB Connection Error:', err));
+
+// Start Server
+const PORT = process.env.PORT || 8002;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 
 
 
