a00b30fabd3d3743dd8c25f16cc159d1a0666022
Nikitajadhav2223 <95409583+Nikitajadhav2223@users.noreply.github.com>
2026-03-31 16:20:00 +0530

    update

--- Routes/index.js
+++ Routes/index.js
@@ -20,3 +20,24 @@ router.post("/login", controller.login);
 
 module.exports=router;
 
+
+
+// const express = require('express');
+// const router = express.Router();
+// const controller = require('../Controllers/index'); // Ensure correct import path
+
+// // Debugging logs
+// console.log("Controller Import:", controller);
+// console.log("getAllProducts:", controller.getAllProducts);
+// console.log("getProductsByTitle:", controller.getProductsByTitle);
+// console.log("getProductsByType:", controller.getProductsByType);
+
+// if (!controller.getAllProducts || !controller.getProductsByTitle || !controller.getProductsByType) {
+//   throw new Error("One or more controller functions are not defined!");
+// }
+
+// router.get('/Products', controller.getAllProducts);
+// router.get('/Products/:Title', controller.getProductsByTitle);
+// router.get('/getProductsByType/:type', controller.getProductsByType);
+
+// module.exports = router;
